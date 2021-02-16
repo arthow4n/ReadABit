95d385bc7a7c25f2546359d23b6087b7b0ac97b0
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-02-16 19:45:25 +0100

    Add dummy backend with "dotnet new"

---
+++ src/server/ReadABit.Web/Controllers/ArticleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReadABit.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok();
+        }
+    }
+}
