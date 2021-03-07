36be14334153a2e472a30a6010c8de88b83d3018
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-03-07 21:25:37 +0100

    Add migration for Words and WordDefinitions

---
+++ src/server/ReadABit.Infrastructure/Migrations/20210307201938_AddWordsAndWordDefinitions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+namespace ReadABit.Infrastructure.Migrations
+{
+    public partial class AddWordsAndWordDefinitions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LanguageCode = table.Column<string>(type: "text", nullable: false),
+                    Expression = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WordDefinitions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WordId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Public = table.Column<bool>(type: "boolean", nullable: false),
+                    LanguageCode = table.Column<string>(type: "text", nullable: false),
+                    Meaning = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<Instant>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WordDefinitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WordDefinitions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WordDefinitions_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleCollections_LanguageCode_Name",
+                table: "ArticleCollections",
+                columns: new[] { "LanguageCode", "Name" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WordDefinitions_LanguageCode",
+                table: "WordDefinitions",
+                column: "LanguageCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WordDefinitions_UserId",
+                table: "WordDefinitions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WordDefinitions_WordId",
+                table: "WordDefinitions",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_LanguageCode_Expression",
+                table: "Words",
+                columns: new[] { "LanguageCode", "Expression" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WordDefinitions");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ArticleCollections_LanguageCode_Name",
+                table: "ArticleCollections");
+        }
+    }
+}
