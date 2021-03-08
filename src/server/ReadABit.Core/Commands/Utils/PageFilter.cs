ad65936887b6cb4192a25b9d9d117bcb99a5ba09
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-03-08 01:29:15 +0100

    Add pagination to query extensions

---
+++ src/server/ReadABit.Core/Commands/Utils/PageFilter.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using FluentValidation;
+
+namespace ReadABit.Core.Commands
+{
+    public interface IPaginatedRequest
+    {
+        public PageFilter Page { get; set; }
+    }
+
+    /// <summary>
+    /// Page filter for incoming requests.
+    /// </summary>
+    public record PageFilter
+    {
+        public int Index { get; set; }
+        public int? Size { get; set; }
+    }
+
+    public class PageFilterValidator : AbstractValidator<PageFilter>
+    {
+        public PageFilterValidator()
+        {
+            RuleFor(x => x.Index).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.Size).GreaterThanOrEqualTo(1).When(x => x.Size is not null);
+            RuleFor(x => x.Size).LessThanOrEqualTo(100).When(x => x.Size is not null);
+        }
+    }
+
+    public class Paginated<T>
+    {
+        public PageInfo Page { get; set; } = new PageInfo { };
+        public List<T> Items { get; set; } = new List<T> { };
+    }
+
+    public record PageInfo
+    {
+        public PageFilterFilled Current { get; set; } = new PageFilterFilled { };
+        public PageFilterFilled? Next { get; set; }
+        public PageFilterFilled? Previous { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+    }
+
+    /// <summary>
+    /// Page filter for retured responses.
+    /// </summary>
+    public record PageFilterFilled : PageFilter
+    {
+        public new int Size { get; set; }
+    }
+}
--- src/server/ReadABit.Core/Commands/Utils/QueryExtensions.cs
+++ src/server/ReadABit.Core/Commands/Utils/QueryExtensions.cs
@@ -1,4 +1,8 @@
+using System.Threading;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using ReadABit.Infrastructure.Models;
 
 namespace ReadABit.Core.Commands
@@ -20,6 +24,35 @@ namespace ReadABit.Core.Commands
                     w.LanguageCode == selector.LanguageCode &&
                     w.Expression == selector.Expression
                 );
+        }
+        public static IQueryable<T> Page<T>(this IQueryable<T> query, PageFilterFilled filter)
+        {
+            return query.Skip((filter.Index - 1) * filter.Size).Take(filter.Size);
+        }
+
+        public static async Task<Paginated<T>> ToPaginatedAsync<T>(this IQueryable<T> query, PageFilter filter, int defaultPageSize, CancellationToken cancellationToken = default)
+        {
+            var totalCount = await query.CountAsync(cancellationToken);
+            var currentFilter = new PageFilterFilled
+            {
+                Index = filter.Index,
+                Size = filter.Size ?? defaultPageSize,
+            };
+            var items = await query.Page(currentFilter).ToListAsync(cancellationToken);
+            var totalPages = decimal.ToInt32(Math.Ceiling((decimal)totalCount / currentFilter.Size));
+
+            return new Paginated<T>
+            {
+                Items = items,
+                Page = new PageInfo
+                {
+                    Current = currentFilter,
+                    Next = currentFilter.Index >= totalPages ? null : currentFilter with { Index = currentFilter.Index + 1 },
+                    Previous = currentFilter.Index < 2 ? null : currentFilter with { Index = currentFilter.Index - 1 },
+                    TotalCount = totalCount,
+                    TotalPages = totalPages,
+                },
+            };
         }
     }
 }
