d7b5f01f06a09e54534fbb530fcbfe5043f7c0c6
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-03-29 19:29:44 +0200

    Change colour code in ArticleScreen

--- src/client/src/navigation/screens/ArticleScreen.tsx
+++ src/client/src/navigation/screens/ArticleScreen.tsx
@@ -35,11 +35,14 @@ import { ArticleStackParamList } from '../navigators/ArticleNavigator.types';
 import { Routes, routeUrl } from '../routes';
 
 const wordFamiliarityLevelColorCodeMapping: Record<string, string> = {
-  0: '#C2D1FF',
-  1: '#FFD4B0',
-  2: '#EBE9A2',
-  3: '#A7D9C0',
-  // #E8AEE1 for red
+  // No colour code for -1 because it's treated as ignored.
+  0: '#E8AEE1', // red
+  1: '#FFD4B0', // orange
+  2: '#A7D9C0', // green
+  // No colour code for 3 because it's the max level, in other words "known word".
+
+  // yellow '#EBE9A2',
+  // blue '#C2D1FF',
 };
 
 const SelectedTokenDefinitionCard: React.FC<{
