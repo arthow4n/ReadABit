0c7cc854bdb11f117d5db05113fcdfee57a1e48d
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-03-31 03:07:54 +0200

    Fix TypeScript path resolving

--- src/client/babel.config.js
+++ src/client/babel.config.js
@@ -2,5 +2,25 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json',
+          ],
+          alias: {
+            '@src': './src',
+          },
+        },
+      ],
+    ],
   };
 };
