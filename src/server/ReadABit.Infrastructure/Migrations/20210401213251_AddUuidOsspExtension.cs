2fd587255ff096e2a850713b909fce697d6461eb
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-04-01 23:33:51 +0200

    Add uuid-ossp extension to PostgreSQL

---
+++ src/server/ReadABit.Infrastructure/Migrations/20210401213251_AddUuidOsspExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReadABit.Infrastructure.Migrations
+{
+    public partial class AddUuidOsspExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP EXTENSION IF EXISTS \"uuid-ossp\";");
+        }
+    }
+}
