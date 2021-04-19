7a169036a80f3b1f802a42c79c1cc08857b9c983
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-04-19 21:52:46 +0200

    Add parse and validation shorthand for NodaTime types

---
+++ src/server/ReadABit.Core/Commands/Utils/TransformExtensions.cs
@@ -0,0 +1,45 @@
+using NodaTime;
+using NodaTime.Text;
+
+namespace ReadABit.Core.Commands.Utils
+{
+    public static class TransformExtensions
+    {
+        public static ParseResult<LocalTime> TryParseIsoHhmmssToLocalTime(this string x)
+        {
+            return LocalTimePattern
+                .CreateWithInvariantCulture("HH':'mm':'ss")
+                .Parse(x);
+        }
+
+        public static LocalTime ParseIsoHhmmssToLocalTimeOrThrow(this string x)
+        {
+            return LocalTimePattern
+                .CreateWithInvariantCulture("HH':'mm':'ss")
+                .Parse(x)
+                .GetValueOrThrow();
+        }
+
+        public static DateTimeZone? TryParseToDateTimeZone(this string x)
+        {
+            return DateTimeZoneProviders.Tzdb.GetZoneOrNull(x);
+        }
+
+        public static DateTimeZone ParseToDateTimeZoneOrThrow(this string x)
+        {
+            return DateTimeZoneProviders.Tzdb[x];
+        }
+
+        public static ZonedDateTime SwapLocalTime(this ZonedDateTime source, LocalTime newLocalTime)
+        {
+            source.Deconstruct(out var localDateTime, out var zone, out var offset);
+            localDateTime.Deconstruct(out var date, out var _);
+
+            return new ZonedDateTime(
+                newLocalTime.On(date),
+                zone,
+                offset
+            );
+        }
+    }
+}
--- src/server/ReadABit.Core/Commands/Utils/ValidationRuleExtensions.cs
+++ src/server/ReadABit.Core/Commands/Utils/ValidationRuleExtensions.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using ReadABit.Core.Commands.Utils;
 using ReadABit.Core.Utils;
 
 namespace ReadABit.Core.Commands
@@ -8,7 +9,21 @@ namespace ReadABit.Core.Commands
         public static IRuleBuilderOptions<T, string> MustBeValidLanguageCode<T>(this IRuleBuilder<T, string> ruleBuilder)
         {
             return ruleBuilder.Must(lc => LanguageCode.IsValid(lc))
-                              .WithMessage("Language code must be a valid IETF BCP 47 language tag, where the shortest form should be preferred when available.");
+                              .WithMessage("Must be a valid IETF BCP 47 language tag, where the shortest form should be preferred when available. For example: en.");
+        }
+
+        public static IRuleBuilder<T, string> MustBeSupportedTimeZone<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                .Must(x => x.TryParseToDateTimeZone() is not null)
+                .WithMessage("Must be a valid IANA time zone name. For example: Asia/Taipei.");
+        }
+
+        public static IRuleBuilder<T, string> MustBeIsoHHmmss<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                .Must(x => x.TryParseIsoHhmmssToLocalTime().Success)
+                .WithMessage("Must be a valid ISO 8601 hh:mm:ss time. For example: 23:59:59.");
         }
 
         public static IRuleBuilderOptions<T, WordSelector> MustBeValidWordSelector<T>(this IRuleBuilder<T, WordSelector> ruleBuilder)
