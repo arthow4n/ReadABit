1f328146fe9d24390437c04385394006401d409e
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-05-09 00:24:30 +0200

    Add missing migration for index on WordFamiliarities used by daily goal

--- src/server/ReadABit.Infrastructure/Migrations/20210508222347_AddIndexForDailyGoalOfWordFamiliarities.Designer.cs
+++ src/server/ReadABit.Infrastructure/Migrations/20210508222347_AddIndexForDailyGoalOfWordFamiliarities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,10 @@ using ReadABit.Infrastructure.Models;
 namespace ReadABit.Infrastructure.Migrations
 {
     [DbContext(typeof(UnsafeCoreDbContext))]
-    partial class UnsafeCoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210508222347_AddIndexForDailyGoalOfWordFamiliarities")]
+    partial class AddIndexForDailyGoalOfWordFamiliarities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/server/ReadABit.Infrastructure/Migrations/20210508222347_AddIndexForDailyGoalOfWordFamiliarities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReadABit.Infrastructure.Migrations
+{
+    public partial class AddIndexForDailyGoalOfWordFamiliarities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_WordFamiliarities_UserId_CreatedAt_Level",
+                table: "WordFamiliarities",
+                columns: new[] { "UserId", "CreatedAt", "Level" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_WordFamiliarities_UserId_CreatedAt_Level",
+                table: "WordFamiliarities");
+        }
+    }
+}
