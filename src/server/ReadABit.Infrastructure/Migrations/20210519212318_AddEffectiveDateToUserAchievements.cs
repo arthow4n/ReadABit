d9d30715b2df889d84ce53eaa4bdf76d9c5ec0fd
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-05-20 19:04:37 +0200

    Fix missing migration SQL for UserAchievement.EffectiveDate

--- src/server/ReadABit.Infrastructure/Migrations/20210519212318_AddEffectiveDateToUserAchievements.cs
+++ src/server/ReadABit.Infrastructure/Migrations/20210519212318_AddEffectiveDateToUserAchievements.cs
@@ -13,6 +13,18 @@ namespace ReadABit.Infrastructure.Migrations
                 type: "date",
                 nullable: false,
                 defaultValue: new NodaTime.LocalDate(1, 1, 1));
+
+            migrationBuilder.Sql($@"
+                WITH r AS (
+                    SELECT ""Id"", ""CreatedAt"", ""EffectiveDate""
+                    FROM ""UserAchievements"" ua
+                    WHERE ua.""EffectiveDate"" = '0001-01-01'
+                )
+                UPDATE ""UserAchievements"" ua
+                SET ""EffectiveDate"" = r.""CreatedAt""::date
+                FROM r
+                WHERE ua.""Id"" = r.""Id"";
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
